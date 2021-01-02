210aa11615e62a933ded452d53fe40ddbd3a8e81
Yiorgos Yiannakoulias <yiorgosynkl@gmail.com>
2021-01-02 17:23:08 +0200

    update script to run different kinds of programs

--- dev/script.py
+++ dev/script.py
@@ -7,10 +7,14 @@ import timeit
 
 start_time = timeit.default_timer()
 
-datasets = ['algae', 'authorship', 'bodyfat', 'calhousing', 'cpu-small', 'fried', 'glass', 'housing', 'iris', 'pendigits', 'segment', 'stock', 'sushi_one_hot', 'sushi', 'vehicle', 'vowel', 'wine', 'wisconsin']
+program_name = ['pairwise_homemade.py', 'pairwise_modules.py', 'pairwise_modules_aggregation.py'][0]
+datasets =  ['algae', 'authorship', 'bodyfat', 'calhousing', 'cpu-small', 'fried', \
+                'glass', 'housing', 'iris', 'pendigits', 'segment', 'stock', 'sushi_one_hot', \
+                'sushi', 'vehicle', 'vowel', 'wine', 'wisconsin']
+
 # ignore = { 0 , 13 }
-ignore = { 0, 3, 5, 9, 12, 13 } # time-consuming datasets: 3, 5, 9, 12 | impossible datasets: 0, 13
-# ignore = { 0, 1, 2, 3, 4, 5, 6, 7, 9, 10, 11, 12, 13, 14, 15, 16, 17 } # time-consuming datasets: 3, 5, 9, 12 | impossible datasets: 0, 13
+# ignore = { 0, 3, 5, 9, 12, 13 } # time-consuming datasets: 3, 5, 9, 12 | impossible datasets: 0, 13
+ignore = { 0, 1, 2, 3, 4, 5, 6, 7, 9, 10, 11, 12, 13, 14, 15, 16, 17 } # time-consuming datasets: 3, 5, 9, 12 | impossible datasets: 0, 13
 
 rows = [['index', 'dataset', 'SVC', 'DTC', 'RFC100', 'SVR', 'DTR', 'RFR100']]
 
@@ -21,7 +25,7 @@ for i, s in enumerate(datasets):
         row = [str(i), s]
         print(f'running dataset {s} ...')
         for c in range(6): # classifier
-            output = subprocess.check_output(f'python3 pairwise_modules.py {i} {c}', shell=True)[:-1].decode("utf-8") 
+            output = subprocess.check_output(f'python3 {program_name} {i} {c}', shell=True)[:-1].decode("utf-8") 
             row.append(output)
         rows.append(row)
 
@@ -32,6 +36,7 @@ print(f'Time required to create results.csv: {run_time} secs')
 lf = open("results_last.csv", "w")
 af = open("results_all.csv", "a")
 af_header = f"""________________________________________________________________
+Program name: {program_name}
 Date: {str(datetime.now())[:19]}
 Runtime: {run_time} secs
 Info: 
