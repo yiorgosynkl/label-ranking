211bb7b245873a709107484fc0589f0a3e918aef
Yiorgos Yiannakoulias <yiorgosynkl@gmail.com>
2021-02-23 15:35:17 +0200

    dataset analysis code, part 1

--- dev/dataset_analysis.py
+++ dev/dataset_analysis.py
@@ -17,9 +17,10 @@ from sklearn.svm import SVC, SVR
 from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
 from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 from sklearn.model_selection import RepeatedKFold
-from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_auc_score, f1_score, accuracy_score
 
 import timeit
+import matplotlib.pyplot as plt
 
 # set random seed
 np.random.seed(0)   # to reproduce same results for random forest
@@ -31,7 +32,9 @@ np.random.seed(0)   # to reproduce same results for random forest
 
 # import dataset
 # csv_choices = ['authorship', 'bodyfat', 'calhousing', 'cpu-small', 'fried', 'glass', 'housing', 'iris', 'pendigits', 'segment', 'stock', 'vehicle', 'vowel', 'wine', 'wisconsin']
-csv_choices = ['fried', 'pendigits', 'segment', 'vowel', 'wisconsin']
+# csv_choices = ['fried', 'pendigits', 'segment', 'vowel', 'wisconsin']
+# csv_choices = ['authorship', 'bodyfat', 'calhousing', 'cpu-small', 'glass', 'housing', 'iris', 'stock', 'vehicle', 'vowel', 'wine', 'wisconsin']
+csv_choices = ['iris']
 
 # s string -> return n_labels and label_names
 def get_labels(s):
@@ -46,8 +49,7 @@ def convert_ranking(rank):
 def convert_ranking_string2list(s):  # label-fixed expression
     return np.argsort(s.split('>'))
 
-
-print('dataset, samples, features , labels, unique rankings, auc_scores, runtime')
+print('dataset, samples, features , labels, unique rankings, auc_scores, f1_scores, accuracy_score, runtime')
 for csv_name in csv_choices:
     start_time = timeit.default_timer()
 
@@ -95,7 +97,7 @@ for csv_name in csv_choices:
         df[mid] = df['ranking'].apply(lambda r: conv(r, i, j))
 
     rkf = RepeatedKFold(n_splits=10, n_repeats=5, random_state=1234)
-    auc_scores = []
+    auc_scores, f1_scores, acc_scores = [], [], []
     for idx_train,idx_test in rkf.split(range(len(df))):
         # step 1: train classifiers
         train_df, test_df = df.loc[idx_train], df.loc[idx_test]
@@ -108,9 +110,65 @@ for csv_name in csv_choices:
             y_pred = list(model.predict(x_test))
             if sum(y_test) != 0 and sum(y_test) != len(y_test):  # not all 0, not all 1
                 auc_scores.append(roc_auc_score(y_test, y_pred))
+            f1_scores.append(f1_score(y_test, y_pred))
+            acc_scores.append(accuracy_score(y_test, y_pred))
 
     stop_time = timeit.default_timer()
     run_time = int(stop_time - start_time)
 
-    out_string += f'{np.round(np.mean(auc_scores),3)}\u00B1{np.round(np.std(auc_scores),3)}, {run_time}s'
+    auc_string_mean = f'{np.round(np.mean(auc_scores),3)}\u00B1{np.round(np.std(auc_scores),3)}'
+    f1_string_mean = f'{np.round(np.mean(f1_scores),3)}\u00B1{np.round(np.std(f1_scores),3)}'
+    acc_string_mean = f'{np.round(np.mean(acc_scores),3)}\u00B1{np.round(np.std(acc_scores),3)}'
+    
+
+    fig, ax = plt.subplots()    
+    plt.hist(auc_scores, int(1/0.02), range=[0.0, 1.0])  # 50 bars
+    ax.set(title=f'histogram of auc scores (50 bars) ({auc_string_mean})\ndataset: {csv_name}', ylabel='number of classifiers', xlabel='roc auc score')
+    plt.savefig(f'dataset_analysis/{csv_name}_50bars_auc.png')
+    plt.close(fig)
+
+    fig, ax = plt.subplots()    
+    plt.hist(f1_scores, int(1/0.02), range=[0.0, 1.0])  # 50 bars
+    ax.set(title=f'histogram of f1 scores (50 bars) ({f1_string_mean})\ndataset: {csv_name}', ylabel='number of classifiers', xlabel='f1 score')
+    plt.savefig(f'dataset_analysis/{csv_name}_50bars_f1.png')
+    plt.close(fig)
+
+    fig, ax = plt.subplots()    
+    plt.hist(acc_scores, int(1/0.02), range=[0.0, 1.0])  # 50 bars
+    ax.set(title=f'histogram of accuracy scores (50 bars) ({acc_string_mean})\ndataset: {csv_name}', ylabel='number of classifiers', xlabel='accuracy score')
+    plt.savefig(f'dataset_analysis/{csv_name}_50bars_accuracy.png')
+    plt.close(fig)
+
+    # # plt.figure()
+    # fig, ax = plt.subplots()    
+    # plt.hist(scores, int(1/0.02))  # 50 bars, zoomed
+    # ax.set(title=f'histogram of auc scores (50 bars, zoomed) ({auc_string_mean})', ylabel='frequency', xlabel='roc auc score')
+    # plt.savefig(f'dataset_analysis/{csv_name}_50bars_zoomed.png')
+    
+    # # plt.figure()
+    # fig, ax = plt.subplots()    
+    # plt.hist(auc_scores, int(1/0.005), range=[0.0, 1.0])  # 200 bars
+    # ax.set(title=f'histogram of auc scores (200 bars) ({auc_string_mean})\ndataset: {csv_name}', ylabel='number of classifiers', xlabel='roc auc score')
+    # plt.savefig(f'dataset_analysis/{csv_name}_200bars_auc.png')  
+    # plt.close(fig)
+
+    # fig, ax = plt.subplots()    
+    # plt.hist(f1_scores, int(1/0.005), range=[0.0, 1.0])  # 200 bars
+    # ax.set(title=f'histogram of f1 scores (200 bars) ({f1_string_mean})\ndataset: {csv_name}', ylabel='number of classifiers', xlabel='f1 score')
+    # plt.savefig(f'dataset_analysis/{csv_name}_200bars_f1.png')  
+    # plt.close(fig)
+
+    # fig, ax = plt.subplots()    
+    # plt.hist(acc_scores, int(1/0.005), range=[0.0, 1.0])  # 200 bars
+    # ax.set(title=f'histogram of accuracy scores (200 bars) ({acc_string_mean})\ndataset: {csv_name}', ylabel='number of classifiers', xlabel='accuracy score')
+    # plt.savefig(f'dataset_analysis/{csv_name}_200bars_accuracy.png')  
+    # plt.close(fig)
+
+    # # plt.figure()
+    # fig, ax = plt.subplots()    
+    # plt.hist(scores, int(1/0.005))  # 200 bars, zoomed
+    # ax.set(title=f'histogram of auc scores (200 bars, zoomed) ({auc_string_mean})', ylabel='frequency', xlabel='roc auc score')
+    # plt.savefig(f'dataset_analysis/{csv_name}_200bars_zoomed.png') 
+
+    out_string += f'{auc_string_mean}, {f1_string_mean}, {acc_string_mean}, {run_time}s'
     print(out_string)
